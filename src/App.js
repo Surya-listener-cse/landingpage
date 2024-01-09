90b060d6ab3d0b313b78cdff70bc963c241823f5
Surya-listener-cse <suryadsp20@gmail.com>
2024-01-09 12:40:45 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+
+import "./App.css";
+
+import Page1 from "./containers/page_1/page_1";
+import Page2 from "./containers/page_2/page_2";
+import Page3 from "./containers/page_3/page_3";
+
+function App() {
+  const [repeat, setRepeat] = useState(true);
+  const recursiveFunc = () => {
+    setRepeat(true);
+    setTimeout(() => {
+      setRepeat(false);
+      recursiveFunc();
+    }, 18000);
+  };
+
+  useEffect(() => {
+    recursiveFunc();
+  }, []);
+
+  return repeat ? (
+    <div className="App">
+      <Page1 />
+      <Page2 />
+      <Page3 />
+    </div>
+  ) : null;
+}
+
+export default App;
